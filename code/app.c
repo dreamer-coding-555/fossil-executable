df9a46049144a2ce888c6591574becd0a6897bce
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-10-21 00:41:52 -0600

    Update app.c

--- code/app.c
+++ code/app.c
@@ -17,6 +17,7 @@
    For more information on the Native Meson Build Application and the Trilobite Coder Lab project, please refer to the project documentation and website.
 */
 #include "app.h"
+#include <SDL.h>
 
 /**
  * Function to run the application with command-line argument parsing.
@@ -26,49 +27,44 @@
  * @return 0 if the --help option is detected and help is printed, EXIT_SUCCESS otherwise.
  */
 int run_app(int argc, char **argv) {
-    TriloOptionParser* parser = trilo_xcore_parser_create_option_parser();
-    
-    // Add options for your program here
-    trilo_xcore_parser_add_option(parser, "enable_logging", TRILO_BOOL_OPTION, 0, 0, "");
-    trilo_xcore_parser_add_option(parser, "threshold", TRILO_INT_OPTION, 0, 100, "");
-    trilo_xcore_parser_add_option(parser, "output_file", TRILO_STRING_OPTION, 0, 0, "");
-    trilo_xcore_parser_add_option(parser, "auto_mode", TRILO_FEATURE_OPTION, 0, 0, "enable,disable,auto");
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    } // end if
 
-    trilo_xcore_parser_process_arguments(parser, argc, argv);
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("Hello SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    } // end if
 
-    // Check and use the options
-    if (trilo_xcore_parser_has_option(parser, "enable_logging")) {
-        printf("Logging is enabled.\n");
-    } else {
-        printf("Logging is disabled.\n");
-    }
+    // Create a renderer for the window
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    } // end if
 
-    if (trilo_xcore_parser_has_option(parser, "threshold")) {
-        int threshold = parser->options[1].int_value;
-        printf("Threshold is set to %d.\n", threshold);
-    }
+    bool quit = false;
+    SDL_Event event;
 
-    if (trilo_xcore_parser_has_option(parser, "output_file")) {
-        const char* outputFile = parser->options[2].str_value;
-        printf("Output file is set to '%s'.\n", outputFile);
-    }
+    while (!quit) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                quit = true; // Exit the loop when the window is closed
+            } // end if
+        } // end while
 
-    if (trilo_xcore_parser_has_option(parser, "auto_mode")) {
-        int autoMode = parser->options[3].feature_value;
-        if (autoMode == 0) {
-            printf("Auto mode is disabled.\n");
-        } else if (autoMode == 1) {
-            printf("Auto mode is enabled.\n");
-        } else {
-            printf("Auto mode is set to 'auto'.\n");
-        }
-    }
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Set the draw color to white
+        SDL_RenderClear(renderer); // Clear the screen
+        SDL_RenderPresent(renderer); // Present the renderer to the window
+    } // end while
 
-    // Clean up
-    for (int i = 0; i < parser->num_options; i++) {
-        free(parser->options[i].str_value);
-    }
-    free(parser->options);
-    free(parser);
+    // Cleanup and quit
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return EXIT_SUCCESS;
 } // end of func
