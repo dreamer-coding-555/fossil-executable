50b1bccca53520c5e051890f60f7a61412db3f22
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-12-06 17:20:02 -0600

    Delete trilobite.py

--- trilobite.py
+++
@@ -1,9 +0,0 @@
-# trilobite.py
-import subprocess
-
-def cube_runner():
-    # Run your Meson application using subprocess
-    subprocess.run(["./builddir/code/app"])
-
-if __name__ == "__main__":
-    cube_runner()
