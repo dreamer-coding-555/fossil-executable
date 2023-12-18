083da0d76c9195fc9313982aa153edbc251d424e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2023-12-17 19:08:19 -0600

    Update app.c

--- code/app.c
+++ code/app.c
@@ -18,23 +18,42 @@
 */
 #include "app.h"
 
-/**
- * Function to run the application with command-line argument parsing.
+/*
+ * The run_app function is designed to read data from a file named "data.txt"
+ * using a custom stream implementation (cstream). It follows these steps:
  *
- * @param argc Number of command-line arguments.
- * @param argv Array of command-line argument strings.
- * @return 0 if the --help option is detected and help is printed, EXIT_SUCCESS otherwise.
+ * 1. Initializes a cstream object named 'io'.
+ * 2. Attempts to open the file "data.txt" in read-only mode.
+ * 3. If the file is successfully opened, reads its content into a buffer
+ *    (up to 255 characters) using the stream_read function.
+ * 4. Null-terminates the buffer to convert it into a valid C string.
+ * 5. Prints the content of the file to the standard output.
+ * 6. Closes the file using stream_close.
+ *
+ * Note: The function assumes that the file contains text data and does not
+ * handle binary files differently. It also does not handle potential errors
+ * during the file operations for simplicity. Consider adding error handling
+ * based on the specific requirements of your application.
+ *
+ * Returns EXIT_SUCCESS if the file is read successfully; otherwise, the
+ * behavior is undefined.
  */
 int run_app(int argc, char **argv) {
     cstream io;
 
-    if (trilo_xcore_stream_open(&io, "data.txt", "r") == 0) {
+    // Attempt to open the file "data.txt" in read-only mode
+    if (stream_open(&io, "data.txt", "r") == 0) {
         char buffer[256];
-        size_t read_count = trilo_xcore_stream_read(&io, buffer, sizeof(char), sizeof(buffer) - 1);
+
+        // Read data from the file into the buffer
+        size_t read_count = stream_read(&io, buffer, sizeof(char), sizeof(buffer) - 1);
         buffer[read_count] = '\0'; // Null-terminate the string
+
+        // Print the content of the file
         printf("Read from file: %s\n", buffer);
 
-        trilo_xcore_stream_close(&io);
+        // Close the file
+        stream_close(&io);
     }
 
     return EXIT_SUCCESS;
